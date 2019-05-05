c1433b1905d34ac37410e2251158e2115f103d28
lizeyuan <476352676@qq.com>
2019-05-05 21:53:44 +0800

    修改文件

--- src/com/MainTest.java
+++ src/com/MainTest.java
@@ -1,15 +1,44 @@
 package com;
-import java.util.Arrays;
+import com.myblog.components.table.mergecells.gridbagtable.GridBagTable;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
 
-public class MainTest {
+public class MainTest  implements ActionListener {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int[] columns = {1,2,3,4,5};
-		System.out.println("getSelectedColumns=" + Arrays.toString(columns));
+	private GridBagTable table;
+	private MainTest()
+	{
+		JFrame d = new JFrame();
+		DefaultTableModel model = new DefaultTableModel(5,5);
+
+		table = new GridBagTable(model);
+		table.setRowHeight(20);
+
+		JScrollPane pane = new JScrollPane(table);
+		d.getContentPane().add(pane, BorderLayout.CENTER);
+		JButton btn = new JButton("合并/拆分");
+		d.getContentPane().add(btn, BorderLayout.NORTH);
+		btn.addActionListener(this);
+		d.setBounds(0, 0, 400, 400);
+		d.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		table.mergeCells(table.getSelectedRows(), table.getSelectedColumns());
+	}
+
+
+	public static void main(String[] fsd){
+		new MainTest();
 	}
 
 }
